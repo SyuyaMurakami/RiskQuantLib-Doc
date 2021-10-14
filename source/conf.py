f97d6bdbe556ddaf9b0e173f467490161db1ffb3
SyuyaMurakami <89246518+SyuyaMurakami@users.noreply.github.com>
2021-10-14 22:09:17 +0800

    Version 31

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,7 @@ project = 'RiskQuantLib'
 author = 'Murakami'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.28'
+release = '0.0.31'
 
 # -- General configuration ---------------------------------------------------
 
